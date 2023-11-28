7b97da6a59472859df423b47fea6aa8b3240d72a
Willian Almeida <willianac01@gmail.com>
2023-11-28 12:15:53 -0300

    feat: add remove button on my-pictures/saved pages

--- src/app/modules/profile/components/saved/saved.component.ts
+++ src/app/modules/profile/components/saved/saved.component.ts
@@ -1,30 +1,12 @@
-import { animate, style, transition, trigger } from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 import { SavedImagesService } from "src/app/core/services/savedimages.service";
 import { IPicture } from "src/app/shared/models/Picture";
 
-const enterTransition = transition(":enter", [
-	style({
-		transform: "translateY(-36px)",
-	}),
-	animate("0.2s linear", style({transform: "translateY(0)"}))
-])
-const leaveTransition = transition(":leave", [
-	style({
-		transform: "translateY(0)"
-	}),
-	animate("0.1s linear", style({transform: "translateY(-36px)"}))
-])
-
-const fadeIn = trigger("enterTr", [enterTransition])
-const fadeOut = trigger("leaveTr", [leaveTransition])
-
 @Component({
 	selector: "app-saved",
 	templateUrl: "./saved.component.html",
-	styleUrls: ["./saved.component.css"],
-	animations: [fadeIn, fadeOut]
+	styleUrls: ["./saved.component.css"]
 })
 export class SavedComponent implements OnInit {
 	userSavedImages$ = this.savedImagesService.returnSavedImages()
